240c6cffb45254cb4a8db054e7e6d21123cfe855
TBA5854 <adhavan5030@gmail.com>
2025-09-11 23:04:00 +0530

    fix: fixed mvvm

--- app/src/main/java/com/tba5854/todo/MainActivity.kt
+++ app/src/main/java/com/tba5854/todo/MainActivity.kt
@@ -9,7 +9,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -22,31 +21,70 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
 import com.tba5854.todo.models.ToDoEntityModel
 import com.tba5854.todo.ui.theme.TodoTheme
-import androidx.activity.viewModels
-import com.tba5854.todo.viewmodels.ToDoViewModel
+import androidx.lifecycle.MutableLiveData
 
 class MainActivity : ComponentActivity() {
-    private val viewModel: com.tba5854.todo.viewmodels.ToDoViewModel by viewModels()
+    private val listOfToDo = MutableLiveData<List<ToDoEntityModel>>()
+
+    private fun initTodos() {
+        listOfToDo.value = listOf(
+            ToDoEntityModel(1, "title1", "description1", false),
+            ToDoEntityModel(2, "title2", "description2", true),
+            ToDoEntityModel(3, "title3", "description3", false),
+            ToDoEntityModel(4, "title4", "description4", true),
+            ToDoEntityModel(5, "title5", "description5", false),
+        )
+    }
+
+    private fun addNewToDo(newToDo: ToDoEntityModel) {
+        val currentList = listOfToDo.value ?: emptyList()
+        listOfToDo.value = currentList + newToDo
+    }
+
+    private fun toggleCompletionStatus(id: Long) {
+        val currentList = listOfToDo.value ?: return
+        listOfToDo.value = currentList.map { toDo ->
+            if (toDo.id == id) toDo.copy(isCompleted = !toDo.isCompleted) else toDo
+        }
+    }
+
+    private fun deleteToDo(id: Long) {
+        val currentList = listOfToDo.value ?: return
+        listOfToDo.value = currentList.filter { it.id != id }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        initTodos()
         setContent {
 
             TodoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-//                    Greeting(
-//                        name = "Android",
-//                        modifier = Modifier.padding(innerPadding)
-//                    )\
-                    Main()
+                    val todos by listOfToDo.observeAsState(initial = emptyList())
+                    Main(
+                        todos = todos,
+                        onToggle = { id -> toggleCompletionStatus(id) },
+                        onDelete = { id -> deleteToDo(id) },
+                        onAdd = {
+                            val nextId = (todos.maxOfOrNull { it.id } ?: 0L) + 1L
+                            addNewToDo(
+                                ToDoEntityModel(
+                                    id = nextId,
+                                    title = "New ToDo $nextId",
+                                    description = "Description $nextId",
+                                    isCompleted = false
+                                )
+                            )
+                        }
+                    )
                 }
             }
         }
@@ -61,72 +99,66 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun Main(viewModel: ToDoViewModel) {
-    val todos by viewModel.listOfToDo.observe(emptyList<ToDoEntityModel>())
+fun Main(
+    todos: List<ToDoEntityModel>,
+    onToggle: (Long) -> Unit,
+    onDelete: (Long) -> Unit,
+    onAdd: () -> Unit,
+) {
     TodoTheme {
-        Scaffold(modifier = Modifier.fillMaxSize(),
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
             topBar = {
                 TopAppBar(
                     title = { Text("To-Do List") },
                 )
             },
             floatingActionButton = {
-                Button(onClick = {}) {
+                Button(onClick = onAdd) {
                     Icon(Icons.Filled.Add, contentDescription = "Add a new todo")
                 }
             }
-            ) { innerPadding ->
+        ) { innerPadding ->
             Column(
-                modifier =Modifier.padding(innerPadding)
+                modifier = Modifier.padding(innerPadding)
             ) {
-
-            ToDoTile(
-                ToDoEntityModel(
-                    1,
-                    "title1",
-                    "description1",
-                    false
-                )
-            )
-                ToDoTile(
-                    ToDoEntityModel(
-                        1,
-                        "title1",
-                        "description1",
-                        false
+                todos.forEach { item ->
+                    ToDoTile(
+                        item = item,
+                        onToggle = onToggle,
+                        onDelete = onDelete
                     )
-                )
+                }
             }
         }
     }
 }
 
-//@PreviewParameter(ToDoPreviewParameterProvider::class)
 @Composable
-fun ToDoTile(item: ToDoEntityModel){
+fun ToDoTile(
+    item: ToDoEntityModel,
+    onToggle: (Long) -> Unit,
+    onDelete: (Long) -> Unit,
+){
     Row (
         modifier = Modifier,
         verticalAlignment = Alignment.CenterVertically
 //            .fillMaxSize()
     ){
         Checkbox(
-            onCheckedChange = { /*TODO*/ },
+            onCheckedChange = { onToggle(item.id) },
             checked = item.isCompleted
         )
         Text(
             item.title,
-//            te,
-//            textAlign = TextAlign.Center,
             modifier = Modifier
                 .weight(1f)
                 .alignByBaseline()
-//            fillMaxWidth()
 
         )
         Button(
-            onClick = { /*TODO*/ }
+            onClick = { onDelete(item.id) }
         ) {
             Icon(Icons.Outlined.Delete, contentDescription =  "Delete Item Button" )
         }
--- app/src/main/java/com/tba5854/todo/views/ToDoPage.kt
+++
@@ -1,3 +0,0 @@
-package com.tba5854.todo.views
-
-//@composable
